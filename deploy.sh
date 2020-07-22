e5d4a41624d9c1f9841f0ec785b2abfa029372fc
yukihane <yukihane.feather@gmail.com>
2020-07-23 08:47:46 +0900

    hugo バージョンアップ

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # If a command fails then the deploy stops
 set -e
 
-alias hugo='docker run --rm -it -u $(id -u $USER) -v $(pwd):/src -v $(pwd)/output:/target klakegg/hugo:0.62.2-ext-asciidoctor'
+alias hugo='docker run --rm -it -u $(id -u $USER) -v $(pwd):/src -v $(pwd)/output:/target klakegg/hugo:0.74.1-ext-asciidoctor'
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
