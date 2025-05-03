b63f25a1d64ef0868fff59fa85e5fc50bf0cc78e
yukihane <yukihane.feather@gmail.com>
2025-05-03 18:40:48 +0900

    出力は分離する

--- tools/mdconvert.sh
+++ tools/mdconvert.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-# 変換したいディレクトリのパスを指定
-DIR_PATH="content"
+# 入力と出力のディレクトリパスを指定
+INPUT_DIR="content"
+OUTPUT_DIR="content_new"
+
+# 出力ディレクトリが存在しない場合は作成
+mkdir -p "$OUTPUT_DIR"
 
 # ディレクトリ内のすべての.adocファイルを検索して変換
-find "$DIR_PATH" -name "*.adoc" -type f | while read -r file; do
-    # 出力ファイルパスを設定
-    xml_file="${file%.adoc}.xml"
-    md_file="${file%.adoc}.md"
-    
+find "$INPUT_DIR" -name "*.adoc" -type f | while read -r file; do
+    # 相対パスを取得
+    rel_path="${file#$INPUT_DIR/}"
+    dir_name=$(dirname "$rel_path")
+
+    # 出力ディレクトリの対応するパスを作成
+    output_dir="$OUTPUT_DIR/$dir_name"
+    mkdir -p "$output_dir"
+
+    # ファイル名の取得（拡張子なし）
+    base_name=$(basename "$file" .adoc)
+
+    # 中間ファイルと出力ファイルのパスを設定
+    xml_file="/tmp/${base_name}_$(date +%s).xml"
+    md_file="$output_dir/${base_name}.md"
+
     echo "変換中: $file"
-    
+
     # DocBookに変換
     asciidoc -b docbook "$file"
-    
+
     # DocBookからMarkdownに変換
     pandoc -f docbook -t markdown "$xml_file" -o "$md_file"
-    
+
     # 中間XMLファイルを削除
     rm "$xml_file"
-    
+
     echo "完了: $md_file"
 done
 
